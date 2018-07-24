aa22d04bd32ad2efb07e06b04fd3dfeb09681d53
JosephTorsney <joetorsney@pop-os.localdomain>
2018-07-24 20:38:10 +0100

    Changed from database to CSV file

--- listener.py
+++ listener.py
@@ -1,22 +1,26 @@
 import json
+import csv
 import tweepy
+from tweetWriter import writeTweet
 from unidecode import unidecode
-from database import Database
 
 
 class Listener(tweepy.streaming.StreamListener):
 
-    def __init__(self):
-        self.db = Database("Tweets")
-
     def on_data(self, data):
-        data = json.loads(data) # Convert into python object
-        text = unidecode(data["text"]) # Decode unicode
-        unix = data["timestamp_ms"]
 
-        self.db.addTweet(unix, text) # Save to database
-        print(unix)
-        print(text + "\n\n###########\n")
+        try:
+            data = json.loads(data) # Convert into python object
+            text = unidecode(data["text"]) # Decode unicode
+            unix = data["timestamp_ms"]
+
+            writeTweet(unix, text)
+            
+            print(unix)
+            print(text + "\n\n###########\n")
+        except KeyError as err:
+            print(str(err))
+
         return True
 
     def on_error(self, status_code):
